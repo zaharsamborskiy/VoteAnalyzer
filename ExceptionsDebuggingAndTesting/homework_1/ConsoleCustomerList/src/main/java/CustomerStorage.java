924573ad10a5b45fae305c22fc6dbee83e9aa4ee
Zahar Samborskiy <mafulon@mail.ru>
2022-11-01 21:41:34 +0700

    Homework 14.10.1

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -14,7 +14,22 @@ public class CustomerStorage {
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
+
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            String smsWrong = "Wrong format! Correct format: add Василий Петров vasily.petrov@gmail.com +79215637722";
+            throw new IllegalArgumentException(smsWrong);
+        }
+        String regexPhone = "\\+79[0-9]{9}";
+        if (!components[INDEX_PHONE].matches(regexPhone)){
+            String smsWrong = "Wrong format! Correct format: +79215637722";
+            throw new IllegalArgumentException(smsWrong);
+        }
+        String regexEmail = "[a-z.]+@[a-z.]+[a-z]+";
+        if (!components[INDEX_EMAIL].matches(regexEmail)) {
+            String smsWrong = "Wrong format! Correct format: vasily.petrov@gmail.com";
+            throw new IllegalArgumentException(smsWrong);
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,6 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
+    private static Logger logger;
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
@@ -12,24 +16,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
-
+        logger = LogManager.getRootLogger();
         while (true) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+                try {
+                    String command = scanner.nextLine();
+                    logger.info("Запрос: " + command);
+                    String[] tokens = command.split("\\s+", 2);
+                    if (tokens[0].equals("add")) {
+                        executor.addCustomer(tokens[1]);
+                    } else if (tokens[0].equals("list")) {
+                        executor.listCustomers();
+                    } else if (tokens[0].equals("remove")) {
+                        executor.removeCustomer(tokens[1]);
+                    } else if (tokens[0].equals("count")) {
+                        System.out.println("There are " + executor.getCount() + " customers");
+                    } else if (tokens[0].equals("help")) {
+                        System.out.println(helpText);
+                    } else {
+                        System.out.println(COMMAND_ERROR);
+                    }
+                }
+                catch (IllegalArgumentException e) {
+                    logger.error(e.getMessage());
+                    System.out.println(e.getMessage());
             }
+                catch (IndexOutOfBoundsException ex){
+                    String smsWrong = "You have not entered data";
+                    logger.error(smsWrong);
+                    System.out.println(smsWrong);
+                }
         }
     }
 }
