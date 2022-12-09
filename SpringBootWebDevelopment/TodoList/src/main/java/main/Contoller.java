c9d537146e41d41718eb62ccdbd7abe3b6e551dc
Zahar Samborskiy <mafulon@mail.ru>
2022-12-09 20:05:27 +0700

    Homework #4

--- SpringBootWebDevelopment/TodoList/src/main/java/main/Contoller.java
+++ SpringBootWebDevelopment/TodoList/src/main/java/main/Contoller.java
@@ -1,15 +1,32 @@
 package main;
 
+import main.model.TodoRepository;
+import main.model.TodoSingle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 
-
+@Controller
 public class Contoller
 {
+    @Autowired
+    private TodoRepository todoRepository;
 
-    public Integer index()
+    @RequestMapping("/")
+    public String index(Model model)
     {
-        return (int) (1000 + Math.random() * 1000);
+        Iterable<TodoSingle> todoIterable = todoRepository.findAll();
+        ArrayList<TodoSingle> arrayTodo = new ArrayList<>();
+        for (TodoSingle todoSingle : todoIterable)
+        {
+            arrayTodo.add(todoSingle);
+        }
+        model.addAttribute("booksCount", arrayTodo.size());
+        model.addAttribute("books", arrayTodo);
+        return "index";
     }
 }
